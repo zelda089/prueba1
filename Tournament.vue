726690bff07d6c424c05ffb027a3daecc07c233e
zelda089 <66275931+zelda089@users.noreply.github.com>
2020-07-27 13:28:51 -0500

    Add files via upload

--- Tournament.vue
+++ Tournament.vue
@@ -37,12 +37,12 @@
                         <!-- Row  -->
                         <div class="row wrap-feature-20 ">
                             <!-- Column -->
-                            <div class="col-lg-6" data-aos="flip-left" data-aos-duration="1200">
+                            <div class="col-lg-6" data-aos="flip-left" data-aos-duration="1200" v-for="(key, Matches, index) in tournaments" :key="index">
                                 <div class="card filter ongoing"  data-aos="zoom-in" data-aos-duration="1200">
-                                    <div class="row each-item">
-                                        <div class="col-md-8">
+                                    <div class="row each-item" >
+                                        <div class="col-md-8" >
                                             <div class="card-body d-flex no-block">
-                                                <div class="m-r-20"><img src="@/assets/images/icono1.png" width="70" class="rounded" /></div>
+                                                <div class="m-r-20"><img :src="key.LogoUrl" width="70" class="rounded" /></div>
                                                 <div>
                                                     <h5 class="font-medium">Football Soccer</h5>
                                                     <p class="descp">Tournament Start Date<b> 15/03/2020 - 8:00pm</b></p>
@@ -50,79 +50,25 @@
                                             </div>
                                         </div>
                                         <div class="col-md-4 text-center">
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal1">Matches <i class="ti-arrow-right"></i></a>
+                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal1"  @click="openModal(Matches)">Matches <i class="ti-arrow-right"></i></a>
 
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal2">Standings <i class="ti-arrow-right"></i></a>
+                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal2" @click="openModal(Matches)">Standings <i class="ti-arrow-right"></i></a>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- Column -->
-                            <!-- Column -->
-                            <div class="col-lg-6" >
-                                <div class="card filter future"  data-aos="zoom-in" data-aos-duration="1200">
-                                    <div class="row each-item">
-                                        <div class="col-md-8">
-                                            <div class="card-body d-flex no-block">
-                                                <div class="m-r-20"><img src="@/assets/images/icono1.png" width="70" class="rounded" /></div>
-                                                <div>
-                                                    <h5 class="font-medium">Football Soccer</h5>
-                                                    <p class="descp">Tournament Start Date<b> 15/03/2020 - 8:00pm</b></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 text-center">
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal1">Matches <i class="ti-arrow-right"></i></a>
 
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal2">Standings <i class="ti-arrow-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Column -->
-                            <!-- Column -->
-                            <div class="col-lg-6" data-aos="zoom-in" data-aos-duration="1200">
-                                <div class="card filter ended"  data-aos="zoom-in" data-aos-duration="1200">
-                                    <div class="row each-item">
-                                        <div class="col-md-8">
-                                            <div class="card-body d-flex no-block">
-                                                <div class="m-r-20"><img src="@/assets/images/icono1.png" width="70" class="rounded" /></div>
-                                                <div>
-                                                    <h5 class="font-medium">Basketball</h5>
-                                                    <p class="descp">Tournament Start Date<b> 15/03/2020 - 8:00pm</b></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 text-center">
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal1">Matches <i class="ti-arrow-right"></i></a>
 
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal2">Standings <i class="ti-arrow-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
-                            <!-- Column -->
-                            <!-- Column -->
-                            <div class="col-lg-6" data-aos="zoom-in" data-aos-duration="1200">
-                                <div class="card filter ongoing"  data-aos="zoom-in" data-aos-duration="1200">
-                                    <div class="row each-item">
-                                        <div class="col-md-8">
-                                            <div class="card-body d-flex no-block">
-                                                <div class="m-r-20"><img src="@/assets/images/icono1.png" width="70" class="rounded" /></div>
-                                                <div>
-                                                    <h5 class="font-medium">Football Soccer</h5>
-                                                    <p class="descp">Tournament Start Date<b> 15/03/2020 - 8:00pm</b></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 text-center">
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal1">Matches <i class="ti-arrow-right"></i></a>
 
-                                            <a href="javascript:void(0)" class="text-white linking bg-success-gradiant" data-toggle="modal" data-target="#mymodal2">Standings <i class="ti-arrow-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <!-- Column -->
+                            <!-- Column -->
+
+                            <!-- Column -->
+                            <!-- Column -->
+
+                            <!-- Column -->
+                            <!-- Column -->
+
                             <!-- Column -->
                         </div>
                         <!-- Row  -->
@@ -135,27 +81,27 @@
             <!-- Modals -->
             <!-- ============================================================== -->
             <!-- modals -->
-            <div id="mymodal1" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
-                <div class="row m-t-40"></div>
+            <div id="mymodal1" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;"  >
+                <div class="row m-t-40"  ></div>
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title" id="myLargeModalLabel">Matches</h4>
+                            <h4 class="modal-title" id="myLargeModalLabel">{{ tournamentSelected.Name }}</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                         </div>
-                        <div class="modal-body">
+                        <div class="modal-body " v-for="(key,value,index) in tournaments" :key="index" >
                             <div class="container">
-                                <div class="col-lg-12 linea">
-                                    <h4 class="tit ">Football Soccer</h4>
-                                    <p>Round 12 Regular Season</p>
+                                <div class="col-lg-12 linea  "   >
+                                    <h4 class="tit "> Football Soccer {{value}}</h4>
+                                    <p> Round 12 Regular Season </p>
 
                                     <img class="sco" src="@/assets/images/score.png">
-                                    <img src="@/assets/images/icono1.png" width="70" class="iconores" />
+                                    <img  :src="tournamentSelected.TournamentLogo" width="70" class="iconores" />
                                     <p class="numres">04</p>
-                                    <p class="texres">Team one</p>
+                                    <p class="texres"> </p>
                                     <p class="numresl">02</p>
-                                    <p class="texresl">Team two</p>
-                                    <p class="loi">Hours:<b> 8:00 am</b> </p>
+                                    <p class="texresl"> </p>
+                                    <p class="loi">Hours:<b>  </b> </p>
                                     <p class="reshou">Venue:<b>School gym</b></p>
 
                                 </div>
@@ -209,44 +155,56 @@
                 </div>
             </div>
         </div>
-        <div id="mymodal2" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
+        <div id="mymodal2" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;" >
             <div class="row m-t-40"></div>
             <div class="modal-dialog modal-lg">
-                <div class="modal-content">
+                <div class="modal-content" >
                     <div class="modal-header">
-                        <h4 class="modal-title" >Standings</h4>
+                        <h4 class="modal-title" >{{ tournamentSelected.Status }}</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body  "   >
                         <div class="container">
-                            <table width="100%" border="1">
-                                <tbody>
-                                <tr>
-                                    <th scope="col">Position</th>
-                                    <th scope="col">Team</th>
+                            <table width="100%" border="1"   >
+
+                                <tbody  v-for="(key,value,index) in tournaments" :key="index">
+
+
+
+
+                                <tr >
+                                    <th scope="col">Position {{value}}</th>
+                                    <th scope="col">Team  </th>
                                     <th scope="col">Points</th>
                                     <th scope="col">Score +</th>
                                     <th scope="col">Score -</th>
 
                                 </tr>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Team 1</td>
-                                    <td>89</td>
+                                <tr >
+                                    <th scope="row" >1</th>
+                                    <td>{{key.Standings[0].TeamName }}</td>
+                                    <td>{{key.Standings[0].Points }}</td>
                                     <td>+63</td>
                                     <td>-14</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">2</th>
-                                    <td>Team 2</td>
-                                    <td>85</td>
+                                    <td> {{key.Standings[1].TeamName }}  </td>
+                                    <td> {{key.Standings[1].Points }}</td>
                                     <td>+78</td>
                                     <td>-25</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">&nbsp;</th>
+                                    <th scope="row">3</th>
+                                    <td>{{key.Standings[2].TeamName }}</td>
+                                    <td> {{key.Standings[2].Points }}</td>
                                     <td>&nbsp;</td>
                                     <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">4</th>
+                                    <td>{{key.Standings[3].TeamName }}</td>
+                                    <td> {{key.Standings[3].Points }}</td>
                                     <td>&nbsp;</td>
                                     <td>&nbsp;</td>
                                 </tr>
@@ -355,13 +313,7 @@
                                     <td>&nbsp;</td>
                                     <td>&nbsp;</td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">&nbsp;</th>
-                                    <td>&nbsp;</td>
-                                    <td>&nbsp;</td>
-                                    <td>&nbsp;</td>
-                                    <td>&nbsp;</td>
-                                </tr>
+
                                 </tbody>
                             </table>
 
@@ -420,9 +372,65 @@
 
 <script>
     import Header from "../components/Header";
+    import axios from "axios";
+
     export default {
+        components: {Header},
+
         name: "Tournament",
-        components: {Header}
+
+        data: () => {
+            return {
+
+
+
+                tournaments: [],
+
+                tournamentSelected: {},
+                tourSelected: {},
+
+                tournament:'',
+                tour:''
+            };
+        },
+
+        mounted() {
+            const university = localStorage.getItem('university');
+            axios.get(`https://tazuz-d281a.firebaseio.com/${university}/Tournaments.json?auth=1DfXFRPwRe9OveIAnKiZUlzXaYb3rozZyHdD1OLA`).then((response) => {
+                console.log(response.data);
+                this.tournaments = response.data;
+
+
+            });
+        },
+
+        methods: {
+            openModal(tournament) {
+                const university = localStorage.getItem('university');
+                console.log(tournament);
+                this.tournament = tournament;
+
+                axios.get(`https://tazuz-d281a.firebaseio.com/${university}/Tournaments/${tournament}.json?auth=1DfXFRPwRe9OveIAnKiZUlzXaYb3rozZyHdD1OLA`).then((response) => {
+
+                    console.log(response.data);
+                    this.tournamentSelected = response.data;
+
+                });
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+
+
+
     }
 </script>
 
